6a632e1c909302a24d016bdc09ae96c35b7787f4
AKKAR Ferdaous <46265098+ferdaousak@users.noreply.github.com>
2022-03-01 20:12:32 +0100

    Initial commit

---
+++ src/main/java/com/dosi/soucleApplicatif/SoucleApplicatifApplication.java
@@ -0,0 +1,13 @@
+package com.dosi.soucleApplicatif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoucleApplicatifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoucleApplicatifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dosi/soucleApplicatif/SoucleApplicatifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dosi.soucleApplicatif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoucleApplicatifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
